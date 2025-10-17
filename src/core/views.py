06b13e173d90ddfb69a82e1b37273636f9e2bc69
silmarillion <bahinoleg@gmail.com>
2025-10-17 13:32:04 +0300

    Point to a correct view

--- src/core/views.py
+++ src/core/views.py
@@ -1,13 +1,12 @@
 from django.conf import settings
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
 
-def index(request):
-    return render(
-        request,
-        "index.html",
-        {
-            "VITE_DEV_SERVER_HOST": settings.VITE_DEV_SERVER_HOST,
-            "DEBUG": settings.DEBUG,
-        }
-    )
+class IndexView(TemplateView):
+    template_name = "index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["VITE_DEV_SERVER_HOST"] = settings.VITE_DEV_SERVER_HOST
+        context["DEBUG"] = settings.DEBUG
+        return context
