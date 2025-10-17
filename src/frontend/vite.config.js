c0597eb5d86d1c106eb50d39c2d3ebf74ceacba2
silmarillion <bahinoleg@gmail.com>
2025-10-17 14:28:14 +0300

    Fix js syntax

--- src/frontend/vite.config.js
+++ src/frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 
 export default defineConfig(({ command }) => ({
-  const host = import.meta.env.VITE_DEV_SERVER_HOST || 'localhost';
+  const host = process.env.VITE_DEV_SERVER_HOST || 'localhost';
 
   plugins: [react()],
 
