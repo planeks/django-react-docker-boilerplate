1b263b58ae207dcb20b1e8a4a0cf0f22618531d4
silmarillion <bahinoleg@gmail.com>
2025-10-17 14:32:09 +0300

    Fix js syntax

--- src/frontend/vite.config.js
+++ src/frontend/vite.config.js
@@ -2,40 +2,36 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 
-export default defineConfig(({ command }) => ({
+export default defineConfig(({ command }) => {
   const host = process.env.VITE_DEV_SERVER_HOST || 'localhost';
 
-  plugins: [react()],
-
-  // Only set base to /static/ in production builds
-  base: command === 'build' ? '/static/' : '/',
-
-  build: {
-    sourcemap: true,
-    manifest: true,
-    outDir: 'dist',
-    rollupOptions: {
-      input: 'src/index.jsx'
-    }
-  },
-
-  server: {
-    host: '0.0.0.0',
-    port: 5173,
-    strictPort: true,
-
-    watch: {
-      usePolling: true,
-      interval: 100,
+  return {
+    plugins: [react()],
+    // Only set base to /static/ in production builds
+    base: command === 'build' ? '/static/' : '/',
+    build: {
+      sourcemap: true,
+      manifest: true,
+      outDir: 'dist',
+      rollupOptions: {
+        input: 'src/index.jsx'
+      }
     },
-
-    cors: true,
-
-    hmr: {
-      protocol: 'ws',
-      host: host,
+    server: {
+      host: '0.0.0.0',
       port: 5173,
-      clientPort: 5173,
+      strictPort: true,
+      watch: {
+        usePolling: true,
+        interval: 100,
+      },
+      cors: true,
+      hmr: {
+        protocol: 'ws',
+        host: host,
+        port: 5173,
+        clientPort: 5173,
+      }
     }
   }
-}))
+})
